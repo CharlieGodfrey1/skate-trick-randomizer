41550a9b02f58085b51bc3ccddd53a7284b58096
CharlieGodfrey1 <47952523+CharlieGodfrey1@users.noreply.github.com>
2020-02-21 13:50:44 +0000

    Cleaning code
    getting rid of unnecessary code.

--- js/index.js
+++ js/index.js
@@ -2,44 +2,32 @@ var easyskate = ["Pop shove it", "Frontside pop shove it", "Front side 180", "Ba
 
 document.getElementById("easy").addEventListener("click", function(){
 	var randomNumber = Math.random();
-	console.log(randomNumber);
 	var randomNumberInRange = randomNumber*easyskate.length;
-	console.log(randomNumberInRange);
 	var randomeIntegerInRange = Math.floor(randomNumberInRange);
-	console.log(randomeIntegerInRange);
 	var trick1 = easyskate[randomeIntegerInRange];
-	console.log(trick1);
 	document.getElementById("quoteDisplay1").innerHTML = trick1;
 	return
 });
 
 
-var mediumskate = ["varial kickflip", "varial heelflip", "backside kickflip", "frontside kickflip"];
+var mediumskate = ["Varial kickflip", "Varial heelflip", "Backside kickflip", "Frontside kickflip"];
 
 document.getElementById("medium").addEventListener("click", function(){
 	var randomNumber = Math.random();
-	console.log(randomNumber);
 	var randomNumberInRange = randomNumber*mediumskate.length;
-	console.log(randomNumberInRange);
 	var randomIntegerInRange = Math.floor(randomNumberInRange);
-	console.log(randomIntegerInRange);
 	var trick2 = mediumskate[randomIntegerInRange];
-	console.log(trick2);
 	document.getElementById("quoteDisplay2").innerHTML = trick2;
 	return
 });
 
-var hardskate = ["impossible", "dolphin flip", "360 flip", "laser flip", "frontside 360", "backside 360"]
+var hardskate = ["Impossible", "Dolphin flip", "360 flip", "Laser flip", "Frontside 360", "Backside 360"]
 
 document.getElementById("hard").addEventListener("click", function(){
 	var randomNumber = Math.random();
-	console.log(randomNumber);
 	var randomNumberInRange = randomNumber*hardskate.length;
-	console.log(randomNumberInRange);
 	var randomeIntegerInRange = Math.floor(randomNumberInRange);
-	console.log(randomeIntegerInRange);
 	var trick3 = hardskate[randomeIntegerInRange];
-	console.log(trick3);
 	document.getElementById("quoteDisplay3").innerHTML = trick3;
 	return
 });
